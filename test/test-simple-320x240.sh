517c4bec8b047b9635cdac4ddfd2125fdd17e6ef
Po-Yu Chuang <ratbert@faraday-tech.com>
2010-12-22 17:24:52 +0800

    test/test-simple-320x240.sh: do not change virtual size

--- test/test-simple-320x240.sh
+++ test/test-simple-320x240.sh
@@ -1,8 +1,6 @@
 # in case we forgot to create device files
 mknod /dev/fb0 c 29 0
 
-echo "320,480" > /sys/class/graphics/fb0/virtual_size
-
 for file in 565_320x240_patterns/*
 do
 	if [ -f $file ]
